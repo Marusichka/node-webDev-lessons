3b74cc9d4eb3124ebcc47ff98411c352d4b25dda
Mariia I <marija.l@borngroup.com>
2022-05-10 00:20:17 +0300

    middleware

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const morgan = require('morgan');
 
 const app = express();
 
@@ -12,6 +13,11 @@ const createPath = (page) => path.resolve(__dirname, 'ejs-views', `${page}.ejs`)
 app.listen(PORT, (error) => {
     error ? console.log(error) : console.log(`listening port ${PORT}`);
 });
+
+// ready middleware for logs
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms'));
+
+app.use(express.static('styles')); // custom - include styles folder
 
 app.get('/', (req, res) => {
     const title = 'Home';
