a6f6e77d21c0e44b53b31fbc96b105cf85cfd0d7
Mariia I <marija.l@borngroup.com>
2022-05-10 14:58:06 +0300

    delete and update post functionality

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
+const methodOverride = require('method-override');
 const Post = require('./models/post');
 const Contact = require('./models/contact');
 
@@ -23,11 +24,13 @@ app.listen(PORT, (error) => {
   error ? console.log(error) : console.log(`listening port ${PORT}`);
 });
 
-app.use(morgan(':method :url :status :res[content-length] - :response-time ms'));
-
 app.use(express.urlencoded({ extended: false }));
 
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms'));
+
 app.use(express.static('styles'));
+
+app.use(methodOverride('_method'));
 
 app.get('/', (req, res) => {
   const title = 'Home';
@@ -38,7 +41,7 @@ app.get('/contacts', (req, res) => {
   const title = 'Contacts';
   Contact
     .find()
-    .then((contacts) => res.render(createPath('contacts'), { contacts, title }))
+    .then(contacts => res.render(createPath('contacts'), { contacts, title }))
     .catch((error) => {
       console.log(error);
       res.render(createPath('error'), { title: 'Error' });
@@ -49,7 +52,42 @@ app.get('/posts/:id', (req, res) => {
   const title = 'Post';
   Post
     .findById(req.params.id)
-    .then((post) => res.render(createPath('post'), { post, title }))
+    .then(post => res.render(createPath('post'), { post, title }))
+    .catch((error) => {
+      console.log(error);
+      res.render(createPath('error'), { title: 'Error' });
+    });
+});
+
+app.delete('/posts/:id', (req, res) => {
+  Post
+    .findByIdAndDelete(req.params.id)
+    .then((result) => {
+      res.sendStatus(200);
+    })
+    .catch((error) => {
+      console.log(error);
+      res.render(createPath('error'), { title: 'Error' });
+    });
+});
+
+app.get('/edit/:id', (req, res) => {
+  const title = 'Edit Post';
+  Post
+    .findById(req.params.id)
+    .then(post => res.render(createPath('edit-post'), { post, title }))
+    .catch((error) => {
+      console.log(error);
+      res.render(createPath('error'), { title: 'Error' });
+    });
+});
+
+app.put('/edit/:id', (req, res) => {
+  const { title, author, text } = req.body;
+  const { id } = req.params;
+  Post
+    .findByIdAndUpdate(id, { title, author, text })
+    .then((result) => res.redirect(`/posts/${id}`))
     .catch((error) => {
       console.log(error);
       res.render(createPath('error'), { title: 'Error' });
@@ -61,11 +99,16 @@ app.get('/posts', (req, res) => {
   Post
     .find()
     .sort({ createdAt: -1 })
-    .then((posts) => res.render(createPath('posts'), { posts, title }))
+    .then(posts => res.render(createPath('posts'), { posts, title }))
     .catch((error) => {
       console.log(error);
       res.render(createPath('error'), { title: 'Error' });
     });
+});
+
+app.get('/add-post', (req, res) => {
+  const title = 'Add Post';
+  res.render(createPath('add-post'), { title });
 });
 
 app.post('/add-post', (req, res) => {
@@ -77,12 +120,7 @@ app.post('/add-post', (req, res) => {
     .catch((error) => {
       console.log(error);
       res.render(createPath('error'), { title: 'Error' });
-    })
-});
-
-app.get('/add-post', (req, res) => {
-  const title = 'Add Post';
-  res.render(createPath('add-post'), { title });
+    });
 });
 
 app.use((req, res) => {
